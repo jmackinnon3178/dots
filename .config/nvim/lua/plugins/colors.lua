3976cd56ad957fda0364f31df0c09499f7242254
josh <joshmackinnon8@gmail.com>
2023-12-20 19:04:52 -0400

    modularised

---
+++ .config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,8 @@
+return {
+	{"mcchrish/zenbones.nvim", lazy = true},
+	{ "EdenEast/nightfox.nvim", lazy = true},
+	{'shaunsingh/nord.nvim', lazy = true},
+	{ "rebelot/kanagawa.nvim", lazy = true },
+	{ 'rose-pine/neovim',name = 'rose-pine', lazy = true },
+
+}
